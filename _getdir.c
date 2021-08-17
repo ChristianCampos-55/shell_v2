c727a3fcdc63b24bca8ca5fe6e8353e02a7ace14
ChristianCampos-55 <gamochris@gmail.com>
2021-08-16 20:18:54 -0500

    [ADD]Refactored files

--- _getdir.c
+++ _getdir.c
@@ -42,3 +42,60 @@ path_node *_getpathdir(char *path, char **en)
 
 	return (head);
 }
+
+/**
+ * _getenv - gets env var information
+ * @variable_env: var to get information for
+ * @en: parsed env variable
+ * Return: var information or NULL
+ */
+
+char *_getenv(const char *variable_env, char **en)
+{
+	int i, j, flag = 1;
+
+	if (en == NULL)
+		return (NULL);
+	for (i = 0; en[i] != NULL; i++, flag = 1)
+	{
+		for (j = 0; en[i][j] != '='; j++)
+		{
+			if (variable_env[j] != en[i][j])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag != 0)
+			return (&(en[i][j + 1]));
+	}
+	return (NULL);
+}
+
+/**
+ * get_path - gets command path
+ * @list_path: linked list of directories
+ * @en: pointer to environment variable
+ * Return: zilch
+ */
+
+void get_path(path_node **list_path, char **en)
+{
+	char *variable_env =  NULL, *copyvari;
+	int i;
+
+	variable_env = _getenv("PATH=", en);
+	if (variable_env == NULL || variable_env[0] == '\0')
+	{
+		*list_path = NULL;
+		return;
+	}
+	for (i = 0; variable_env[i] != '\0'; i++)
+		;
+	copyvari = malloc(sizeof(char) * (i + 1));
+	for (i = 0; variable_env[i] != '\0'; i++)
+		copyvari[i] = variable_env[i];
+	copyvari[i] = '\0';
+
+	*list_path = _getpathdir(copyvari, en);
+}
---
+++ commands_exe.c
@@ -0,0 +1,95 @@
+#include "shell_header.h"
+
+/**
+* create_pipe - genesis
+* @pipefd: array of genesis
+* Return: status
+*/
+
+int create_pipe(int (*pipefd)[2])
+{
+	if (pipe(*pipefd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
+
+/**
+* read_command_output - genesis
+* @pipefd: array of genesis
+* @cur_node: current node
+* Return: status
+*/
+
+int read_command_output(int *pipefd, dlistint_t *cur_node)
+{
+	char buff[1025] = "";
+	int i = 0;
+
+	close(pipefd[1]);
+	for (i = 0; i < 1025; i++)
+		buff[i] = '\0';
+	while (read(pipefd[0], &buff, 1024) > 0)
+	{
+		realloc_buffer(&(cur_node->buffer_in), buff);
+		for (i = 0; i < 1025; i++)
+			buff[i] = '\0';
+	}
+	close(pipefd[0]);
+	return (0);
+}
+
+/**
+* change_output_command - genesis
+* @pipefd: array of genesis
+* Return: status
+*/
+
+int change_output_command(int *pipefd)
+{
+	close(pipefd[0]);
+	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		return (EXIT_FAILURE);
+	}
+	close(pipefd[1]);
+	return (0);
+}
+
+/**
+* realloc_buffer - add com to buffer
+* @buffer: command upon execution
+* @str: new com to add
+* Return: status
+*/
+
+int realloc_buffer(char **buffer, char *str)
+{
+	char *new_buffer = NULL;
+	int size_buffer = 0, size_str = 0, i = 0;
+
+	if (!str || str[0] == '\0')
+		return (0);
+	for (size_buffer = 0; *buffer && (*buffer)[size_buffer]; size_buffer++)
+		continue;
+	for (size_str = 0; str && str[size_str]; size_str++)
+		continue;
+	new_buffer = malloc(sizeof(char) * (size_str + size_buffer + 1));
+	if (!new_buffer)
+		return (1);
+	for (i = 0; i < size_buffer + size_str; i++)
+	{
+		if (i < size_buffer)
+			new_buffer[i] = (*buffer)[i];
+		else
+			new_buffer[i] = str[i - size_buffer];
+	}
+	new_buffer[i] = '\0';
+	if (*buffer)
+		free(*buffer);
+	*buffer = new_buffer;
+	return (0);
+}
